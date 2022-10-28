6565d3f237b606a993bddfd824f84b3b128a48a5
chris <selfawarecpu21@gmail.com>
2022-10-27 20:03:29 -0500

    initial

---
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/TechJobsController.java
@@ -0,0 +1,15 @@
+package org.launchcode.techjobs.mvc.controllers;
+
+import java.util.HashMap;
+
+public class TechJobsController {
+
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public TechJobsController(){
+
+    }
+
+
+
+}
